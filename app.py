b46bd06ca90b422cbc5ebb0599aa4e74d360e742
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2024-02-02 02:32:56 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import g4f
 import g4f.api
 import os
 
-ENV_PORT = os.getenv('PORT') if os.getenv('PORT') in os.getenv('PORT') else 3000
+ENV_PORT = 5000 if os.getenv('PORT') else 5000
 
 def main():
     print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
-    g4f.api.Api(engine = g4f, debug = True).run(ip = "0.0.0.0:{ENV_PORT}")
+    g4f.api.Api(engine = g4f, debug = False).run(ip = f'0.0.0.0:{ENV_PORT}')
 
 if __name__ == "__main__":
     main()
-
-
