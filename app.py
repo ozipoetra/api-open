e9a57b0f6356c1f3bd6ea12df35057091a73b37e
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2024-02-03 13:54:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import g4f
-import g4f.api
+from g4f.api import Api
 import os
 
 ENV_PORT = os.getenv('PORT')
-app = g4f.api()
+app = Api(engine = g4f, debug = False).run(ip = f'0.0.0.0:{ENV_PORT}')
 
 def main():
     print(f'Starting server... [g4f v-{g4f.version.utils.current_version}]')
-    app.Api(engine = g4f, debug = False).run(ip = f'0.0.0.0:{ENV_PORT}')
+    app
 
 if __name__ == "__main__":
     main()
